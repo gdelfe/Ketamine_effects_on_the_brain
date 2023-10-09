64e5a8fe5288b588bd43f17e9d1997714285710d
gdelfe <fentonlab@HV2S0R3.ad.nyu.edu>
2023-10-09 12:07:11 -0400

    added functions for filtering lfp, masking, etc...

--- python/utils_general.py
+++ python/utils_general.py
@@ -85,7 +85,7 @@ def load_data(binFullPath,HPC_path_file,PFC_path_file,brain_reg,sess):
         print(f'Gain factors all the same for each Lfp channel: {gain[0]} in Volts')
         gain = gain[0] 
     else:
-        print('Gain factor multiplication should be done channel by channel, this is just a bit slower')
+        sys.exit('Gain factor multiplication should be done channel by channel, this is just a bit slower')
         
         
     #### Read LFP start/end time, HPC channels - Choose whether loading HPC or PFC here
@@ -135,7 +135,7 @@ def load_data(binFullPath,HPC_path_file,PFC_path_file,brain_reg,sess):
     print('Length in minutes of Lfp recordings: ',(Lfp.shape[0])/2500/60)
     print('Difference in seconds: ',((Lfp.shape[0])/2500/60 - (speed[sess].size)/100/60)*60)
 
-    return Lfp, speed 
+    return Lfp, speed, gain
 
 
 
@@ -170,13 +170,11 @@ def split_into_epochs(Lfp,speed_up,N=2500):
 # =============================================================================
 
 
-def select_1min_data(current_min, N=2500):
+def select_1min_data(Lfp_B, Lfp_L, Lfp_M, Lfp_H, speed_B, speed_L, speed_M, speed_H, current_min, N=2500):
     
     #### 3. Select 1 min of data for each Epoch, for both Lfp and speed
     ##### Select 1 every M channels for the Lfp
 
-    # parameters
-    current_min = 0 # minute to look at
 
     M = 1 # keep every M channel
     length = 1 # length of period to look at, i.e. 1 = 1 min 
@@ -198,9 +196,194 @@ def select_1min_data(current_min, N=2500):
     speed_H_min = speed_H[start:end]
 
 
-    Lfp_B_min.shape, speed_B_min.shape, speed_B_min.size/N
+    print('Lfp shape {}, speed shape {}, length in sec: {}'.format(Lfp_B_min.shape, speed_B_min.shape, speed_B_min.size/N))
+    
+    return Lfp_B_min, Lfp_L_min, Lfp_M_min, Lfp_H_min, speed_B_min, speed_L_min, speed_M_min, speed_H_min
+         
+
+# =============================================================================
+
+    
+def decimate_lfp_and_speed(lfp_filt_B,lfp_filt_L,lfp_filt_M,lfp_filt_H,speed_B_min,speed_L_min,speed_M_min,speed_H_min):
     
     
+    # decimated Lfp to 1250 Hz
+    lfp_dec_B = lfp_filt_B[::2,:]
+    lfp_dec_L = lfp_filt_L[::2,:]
+    lfp_dec_M = lfp_filt_M[::2,:]
+    lfp_dec_H = lfp_filt_H[::2,:]
+    
+    # decimated speed to 1250 Hz
+    speed_dec_B = speed_B_min[::2]
+    speed_dec_L = speed_L_min[::2]
+    speed_dec_M = speed_M_min[::2]
+    speed_dec_H = speed_H_min[::2]
+    
+    return lfp_dec_B, lfp_dec_L, lfp_dec_M, lfp_dec_H, speed_dec_B, speed_dec_L, speed_dec_M, speed_dec_H
+
+# =============================================================================
+
+
+# input: 
+# lfp_dec: lfp (subsampled)
+# win: time length of the window we are looking at in time points, i.e. 1 sec = 1250 points
+# std_th: std threshold
+# period: string for baseline, low, etc..
+
+def lfp_artifacts_mask(lfp_dec,win,std_th):
+
+    zlfp = zscore(lfp_dec,axis=0)
+    mask_lfp = np.abs(zlfp) < std_th
+
+    nch = lfp_dec.shape[1] # num of channels
+    T_length = lfp_dec.shape[0] # time series length 
+
+    mask_trial = [[] for ch in range(nch)] # create mask for each channel, per trial
+    mask = [[] for ch in range(nch)] # create mask for each channel, per time point  
+    good_trial_cnt = []
+    
+    for ch in range(nch): # for each channel 
+        cnt = 0 # count 
+        for i in range(0,T_length,win): # for time length
+            data_win = mask_lfp[i:i+win,ch]
+            if np.all(data_win): # if there are no artifact within the win considered
+                mask_trial[ch].extend([True]) # create True mask for that window, per trial
+                mask[ch].extend([1]*win) # create True mask for that window, per time point 
+                cnt +=1
+            else:
+                mask_trial[ch].extend([False]) # create False mask for that window, per trial
+                mask[ch].extend([-1]*win) # create True mask for that window, per time point 
+        
+        good_trial_cnt.append(cnt/(T_length/win)) # cnt n. of good trials in each channel 
+
+    mask_trial_arr = np.array(mask_trial).T
+    mask_arr = np.array(mask).T
+    good_trial_arr = np.array(good_trial_cnt)
+    
+    return mask_trial_arr, mask_arr, good_trial_arr
+
+
+# =============================================================================
+
+# Create a speed mask. 
+
+# True if speed < threshold at any point in a given time window of length win
+# False if there is at least one value above threshold in the given time window
+# Input: 
+# speed_up: upsampled speed
+# win: amplitude in data points of the win we are looking at for thresholding
+# thresh: speed threshold for speed
+# period: string for baseline, low, mid, high injection 
+
+def create_speed_mask(speed_up,win,thresh,level,period):
+    mask = []
+    if level == 'low':
+        speed_th = speed_up < thresh # speed mask below threshold
+    elif level == 'high':
+        speed_th = speed_up > thresh # speed mask below threshold
+    else:
+        sys.exit('Wrong speed scenario, choose either low or high for level')
+            
+    cnt = 0 # count for low speed trials 
+    for i in range(0,len(speed_th),win):
+        data_win = speed_th[i:i+win]
+        if np.all(data_win): # if all the speed value are below threshold
+            mask.extend([True]) # create True mask for that window
+            cnt +=1
+        else:
+            mask.extend([False]) # create False mask for that window
+    print("{} speed trials % in {} : {:.2f}".format(level,period,cnt/((len(speed_th)/win))))
+    return np.array(mask)
+
+
+# =============================================================================
+
+
+# Create mask for:
+#     1. Lfp artifacts
+#     2. speed (low and high speed)
+#     3. Combine them together
+
+# win = window length, e.g. 1250 = 1 sec
+# th = speed threshold 
+    
+def make_speed_and_lfp_maks(lfp_dec_B,lfp_dec_L, lfp_dec_M, lfp_dec_H, speed_dec_B, speed_dec_L, speed_dec_M, speed_dec_H, win = 1250, th = 30):
+    
+    nch = lfp_dec_B.shape[1] # numb of channels 
+    
+    # =============================================================================
+    #     LFP mask for artifacts 
+    # =============================================================================
+        
+    # Create mask for artifact trials in Lfp
+
+    mask_trial_B, lfp_mask_B, good_trial_rate_B = lfp_artifacts_mask(lfp_dec_B,win,4)
+    mask_trial_L, lfp_mask_L, good_trial_rate_L = lfp_artifacts_mask(lfp_dec_L,win,4)
+    mask_trial_M, lfp_mask_M, good_trial_rate_M = lfp_artifacts_mask(lfp_dec_M,win,4)
+    mask_trial_H, lfp_mask_H, good_trial_rate_H = lfp_artifacts_mask(lfp_dec_H,win,4)
+
+    # =============================================================================
+    # Speed masks for Low and High speed
+    # =============================================================================
+
+    # make a speed map for low speed
+    speed_mask_B_low = create_speed_mask(speed_dec_B,win,th,'low','base')
+    speed_mask_L_low = create_speed_mask(speed_dec_L,win,th,'low','low')
+    speed_mask_M_low = create_speed_mask(speed_dec_M,win,th,'low','mid')
+    speed_mask_H_low = create_speed_mask(speed_dec_H,win,th,'low','high')
+    print()
+    # make a speed map for low speed
+    speed_mask_B_high = create_speed_mask(speed_dec_B,win,th,'high','base')
+    speed_mask_L_high = create_speed_mask(speed_dec_L,win,th,'high','low')
+    speed_mask_M_high = create_speed_mask(speed_dec_M,win,th,'high','mid')
+    speed_mask_H_high = create_speed_mask(speed_dec_H,win,th,'high','high')
+    
+    # =============================================================================
+    # Combine speed mask and Lfp artifact mask
+    # =============================================================================
+    
+    # Combine the lfp good trial mask with the low speed mask
+    tot_mask_B_low_s = (mask_trial_B.T & speed_mask_B_low).T
+    tot_mask_L_low_s = (mask_trial_L.T & speed_mask_L_low).T
+    tot_mask_M_low_s = (mask_trial_M.T & speed_mask_M_low).T
+    tot_mask_H_low_s = (mask_trial_H.T & speed_mask_H_low).T
+    
+    # cnt = np.sum(mask_trial_L,axis=0)
+    # cnt_tot = np.sum(tot_mask_L_low_s,axis=0)
+    # cnt - cnt_tot # mask difference, must be >= 0
+    
+    # print('\nNo artifact trials\n ',cnt)
+    # print('\nNo artifact, low speed trials \n ',cnt_tot)
+    
+    # Combine the lfp good trial mask with the high speed mask
+    tot_mask_B_high_s = (mask_trial_B.T & speed_mask_B_high).T
+    tot_mask_L_high_s = (mask_trial_L.T & speed_mask_L_high).T
+    tot_mask_M_high_s = (mask_trial_M.T & speed_mask_M_high).T
+    tot_mask_H_high_s = (mask_trial_H.T & speed_mask_H_high).T
+    
+    # cnt = np.sum(mask_trial_M,axis=0)
+    # cnt_tot = np.sum(tot_mask_M_high,axis=0)
+    # cnt - cnt_tot # mask difference, must be >= 0
+    
+    # print('\nNo artifact trials\n ',cnt)
+    # print('\nNo artifact, high speed trials \n ',cnt_tot)
+    
+    # low speed trial rate
+    tot_good_trial_rate_B = np.sum(np.sum(tot_mask_B_low_s,axis=0))/60/nch
+    tot_good_trial_rate_L = np.sum(np.sum(tot_mask_L_low_s,axis=0))/60/nch
+    tot_good_trial_rate_M = np.sum(np.sum(tot_mask_M_low_s,axis=0))/60/nch
+    tot_good_trial_rate_H = np.sum(np.sum(tot_mask_H_low_s,axis=0))/60/nch
+    print('good trial-low speed rate, base = {:.2f}, low = {:.2f}, mid = {:.2f}., high = {:.2f}'.format(tot_good_trial_rate_B,tot_good_trial_rate_L,tot_good_trial_rate_M,tot_good_trial_rate_H))
+    
+    # high speed trial rate
+    tot_good_trial_rate_B = np.sum(np.sum(tot_mask_B_high_s,axis=0))/60/nch
+    tot_good_trial_rate_L = np.sum(np.sum(tot_mask_L_high_s,axis=0))/60/nch
+    tot_good_trial_rate_M = np.sum(np.sum(tot_mask_M_high_s,axis=0))/60/nch
+    tot_good_trial_rate_H = np.sum(np.sum(tot_mask_H_high_s,axis=0))/60/nch
+    print('good trial-high speed rate, base = {:.2f}, low = {:.2f}, mid = {:.2f}., high = {:.2f}'.format(tot_good_trial_rate_B,tot_good_trial_rate_L,tot_good_trial_rate_M,tot_good_trial_rate_H))
+    
+    
+    return tot_mask_B_low_s, tot_mask_L_low_s, tot_mask_M_low_s, tot_mask_H_low_s, tot_mask_B_high_s, tot_mask_L_high_s, tot_mask_M_high_s,tot_mask_H_high_s 
 
 
 
--- python/utils_plotting.py
+++ python/utils_plotting.py
@@ -19,7 +19,7 @@ def plot_speed(speed_up,sess,start,end,Xtick,fs_lfp=2500,fs_beh=100):
     s = fs_lfp*start # star
     e = fs_lfp*end # end
     
-    print('Number of seconds: ',(e-s)/fs_lfp)
+    # print('Number of seconds: ',(e-s)/fs_lfp)
     
     plt.figure(figsize=(12,5))
     
@@ -175,5 +175,30 @@ def plot_lfp_various_channels(Lfp,ch1,ch2,start,end,P,Q,Xtick,N=2500):
     plt.tight_layout()
     plt.show()
 
+# =============================================================================
 
 
+def plot_filtered_lfp(lfp_filt, start, end, Xtick, ch, N = 2500):
+    
+    
+    L_start = start*N # start of time series in sec
+    L_end = end*N # end of time series 
+    T = Xtick*N # xtick period 
+    
+    
+    plt.figure(figsize=(10,5))
+    plt.plot(lfp_filt[L_start:L_end,ch],linewidth=0.5)
+    # plt.plot(lfp_filt_B2[L_start:L_end,ch],linewidth=0.5)
+    plt.title(f'LFP for channel {ch}',fontsize=12)
+    plt.xlabel('time (sec)',fontsize=12)
+    plt.ylabel('$\mu$V',fontsize=12)
+    
+    
+    xticks = np.arange(0,L_end-L_start, step=T)
+    plt.xticks(ticks=xticks, labels=[str(i/N) for i in xticks],fontsize=10)
+    plt.yticks(fontsize=10)
+    # print(xticks)
+    plt.tight_layout()
+    plt.show()
+    
+    
