62410d8dfef52066fdca7dfc8fd7e027e4268283
Luke Arend <larend@flatironinstitute.org>
2024-05-21 01:04:38 -0400

    add 3_normalize_spikes which packages raw spike data into dataframes

--- bin/spikecopy.sh
+++ bin/spikecopy.sh
@@ -1,57 +1,57 @@
 #!/bin/bash
 
 # 2022-07-27:0
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_0.file" "2022-07-27-hpc-spikes.pkl"
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_0.file" "2022-07-27-pfc-spikes.pkl"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_0.file" "hpc/spk_250Hz_0.file"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_0.file" "pfc/spk_250Hz_0.file"
 
 # 2022-07-28:1
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_1.file" "2022-07-28-hpc-spikes.pkl"
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_1.file" "2022-07-28-pfc-spikes.pkl"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_1.file" "hpc/spk_250Hz_1.file"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_1.file" "pfc/spk_250Hz_1.file"
 
 # 2022-08-01:2
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_2.file" "2022-08-01-hpc-spikes.pkl"
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_2.file" "2022-08-01-pfc-spikes.pkl"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_2.file" "hpc/spk_250Hz_2.file"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_2.file" "pfc/spk_250Hz_2.file"
 
 # 2022-08-03:3
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_3.file" "2022-08-03-hpc-spikes.pkl"
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_3.file" "2022-08-03-pfc-spikes.pkl"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_3.file" "hpc/spk_250Hz_3.file"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_3.file" "pfc/spk_250Hz_3.file"
 
 # 2022-08-04:4
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_4.file" "2022-08-04-hpc-spikes.pkl"
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_4.file" "2022-08-04-pfc-spikes.pkl"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_4.file" "hpc/spk_250Hz_4.file"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_4.file" "pfc/spk_250Hz_4.file"
 
 # 2022-08-06:5
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_5.file" "2022-08-06-hpc-spikes.pkl"
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_5.file" "2022-08-06-pfc-spikes.pkl"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_5.file" "hpc/spk_250Hz_5.file"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_5.file" "pfc/spk_250Hz_5.file"
 
 # 2022-08-08:6
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_6.file" "2022-08-08-hpc-spikes.pkl"
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_6.file" "2022-08-08-pfc-spikes.pkl"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_6.file" "hpc/spk_250Hz_6.file"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_6.file" "pfc/spk_250Hz_6.file"
 
 # 2022-08-11:7
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_7.file" "2022-08-11-hpc-spikes.pkl"
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_7.file" "2022-08-11-pfc-spikes.pkl"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_7.file" "hpc/spk_250Hz_7.file"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_7.file" "pfc/spk_250Hz_7.file"
 
 # 2022-08-12:8
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_8.file" "2022-08-12-hpc-spikes.pkl"
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_8.file" "2022-08-12-pfc-spikes.pkl"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_8.file" "hpc/spk_250Hz_8.file"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_8.file" "pfc/spk_250Hz_8.file"
 
 # 2022-08-13:9
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_9.file" "2022-08-13-hpc-spikes.pkl"
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_9.file" "2022-08-13-pfc-spikes.pkl"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_9.file" "hpc/spk_250Hz_9.file"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_9.file" "pfc/spk_250Hz_9.file"
 
 # 2022-08-15:10
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_10.file" "2022-08-15-hpc-spikes.pkl"
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_10.file" "2022-08-15-pfc-spikes.pkl"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_10.file" "hpc/spk_250Hz_10.file"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_10.file" "pfc/spk_250Hz_10.file"
 
 # 2022-09-12:15
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_15.file" "2022-09-12-hpc-spikes.pkl"
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_15.file" "2022-09-12-pfc-spikes.pkl"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_15.file" "hpc/spk_250Hz_15.file"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_15.file" "pfc/spk_250Hz_15.file"
 
 # 2022-09-14:16
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_16.file" "2022-09-14-hpc-spikes.pkl"
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_16.file" "2022-09-14-pfc-spikes.pkl"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_16.file" "hpc/spk_250Hz_16.file"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_16.file" "pfc/spk_250Hz_16.file"
 
 # 2022-09-16:17
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_17.file" "2022-09-16-hpc-spikes.pkl"
-sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_17.file" "2022-09-16-pfc-spikes.pkl"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/HPC/spk_250Hz_17.file" "hpc/spk_250Hz_17.file"
+sshpass -f monkauth scp "/f/fentonlab/RAWDATA/NeuroPix/spk_ketamine/PFC/spk_250Hz_17.file" "pfc/spk_250Hz_17.file"
