9afc256950d9652b44fb87cca29dfaf1124d2904
Luke Arend <larend@flatironinstitute.org>
2024-05-26 23:13:34 -0400

    move around notebooks, add vidcopy

---
+++ bin/vidcopy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# 2022-09-16:17
+sshpass -f monkauth scp "lukea@monk.cns.nyu.edu:/f/fentonlab/RAWDATA/NeuroPix/Ketamine/2022-09-16-02-10-00_M023_RSK_mPFC_HPC_3_10_30mpk/0916TrackerVideo_info.txt" "0916TrackerVideo_info.txt"
+sshpass -f monkauth scp "lukea@monk.cns.nyu.edu:/f/fentonlab/RAWDATA/NeuroPix/Ketamine/2022-09-16-02-10-00_M023_RSK_mPFC_HPC_3_10_30mpk/0916TrackerVideo.avi" "0916TrackerVideo.avi"
